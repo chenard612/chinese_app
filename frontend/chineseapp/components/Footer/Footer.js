338dc4f9e62289a49961c2ad20232aa66f389f51
chenard612 <chenard612@hotmail.com>
2022-10-31 09:28:03 -0400

    footer is empty

--- frontend/chineseapp/components/Footer/Footer.js
+++ frontend/chineseapp/components/Footer/Footer.js
@@ -3,7 +3,7 @@ import styles from '../../styles/components/Footer.module.scss'
 const Footer = () => {
     return <div className={styles.container}>
             <div className={styles.compartment}>
-                Footer
+                
             </div>
             </div>
 }
